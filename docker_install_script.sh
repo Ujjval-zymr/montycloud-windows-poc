82354ba7934a7137bac406b8ce2ccddd15692adc
Ujjval Darji <ujjval.darji@zymr.com>
2023-05-08 15:45:01 +0530

    aws cli install cmd added

--- docker_install_script.sh
+++ docker_install_script.sh
@@ -20,16 +20,20 @@ Install-Package -Name docker -ProviderName DockerMsftProvider -Force
 # Checking Status of Docker
 docker version
 
+# Download the AWS CLI installer
+Invoke-WebRequest -Uri "https://awscli.amazonaws.com/AWSCLIV2.msi" -OutFile "$env:USERPROFILE\Downloads\AWSCLIV2.msi"
+
+# Install the AWS CLI
+Start-Process msiexec -Wait -ArgumentList "/i `"$env:USERPROFILE\Downloads\AWSCLIV2.msi`" /quiet"
+
+# Verify that the AWS CLI is installed
+aws --version
+
 # Install SSM Agent
 Write-Host "Downloading SSM Agent installer"
 Invoke-WebRequest "https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/windows_amd64/AmazonSSMAgentSetup.exe" -OutFile "C:\AmazonSSMAgentSetup.exe"
 Write-Host "Installing SSM Agent"
 Start-Process -FilePath "C:\AmazonSSMAgentSetup.exe" -ArgumentList @("/install", "/quiet") -Wait
-
-# # Configure SSM Agent to allow connections
-# Write-Host "Configuring SSM Agent to allow connections"
-# Set-ItemProperty -Path "HKLM:\SOFTWARE\Amazon\SSM" -Name "AllowRunCommand" -Value "true"
-# Set-ItemProperty -Path "HKLM:\SOFTWARE\Amazon\SSM" -Name "AllowSessionManager" -Value "true"
 
 # Start SSM Agent service
 Write-Host "Starting SSM Agent service"
