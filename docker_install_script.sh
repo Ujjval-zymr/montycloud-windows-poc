cda829cf67a09e5778965801bc9d0269042f91a1
Ujjval Darji <ujjval.darji@zymr.com>
2023-05-08 13:07:36 +0530

    added ssm agent download and install script

--- docker_install_script.sh
+++ docker_install_script.sh
@@ -1,6 +1,5 @@
 # prerequisites 
 # Docker EE required Windows Server 2016 and higher
-# Windows server STD 2016 - 10.0.14393 N/A Build 14393
 # powershell version 5
 <powershell>
 set-executionpolicy -scope CurrentUser -executionPolicy Bypass -Force
@@ -11,9 +10,34 @@ If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
     exit
 }
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-#Install Docker Provider
+
+# Install Docker Provider
 Install-Package -Name DockerMsftProvider -Force
-#Install Docker from Docker Provider
+
+# Install Docker from Docker Provider
 Install-Package -Name docker -ProviderName DockerMsftProvider -Force
+
+# Checking Status of Docker
+docker version
+
+# Install SSM Agent
+Write-Host "Downloading SSM Agent installer"
+Invoke-WebRequest "https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/windows_amd64/AmazonSSMAgentSetup.exe" -OutFile "C:\AmazonSSMAgentSetup.exe"
+Write-Host "Installing SSM Agent"
+Start-Process -FilePath "C:\AmazonSSMAgentSetup.exe" -ArgumentList @("/install", "/quiet") -Wait
+
+# # Configure SSM Agent to allow connections
+# Write-Host "Configuring SSM Agent to allow connections"
+# Set-ItemProperty -Path "HKLM:\SOFTWARE\Amazon\SSM" -Name "AllowRunCommand" -Value "true"
+# Set-ItemProperty -Path "HKLM:\SOFTWARE\Amazon\SSM" -Name "AllowSessionManager" -Value "true"
+
+# Start SSM Agent service
+Write-Host "Starting SSM Agent service"
+Start-Service -Name "AmazonSSMAgent"
+Set-Service -Name "AmazonSSMAgent" -StartupType Automatic
+
+# Checking SSM Agent status
+Get-Service AmazonSSMAgent
+
 Restart-Computer -Force
 </powershell>
